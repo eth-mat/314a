f8a9d01000301325cff52f9abdc6a894314fd343
Ethan <e.c.matsubayashi@wustl.edu>
2025-04-08 18:01:47 -0500

    Code to retrieve state .csv's functional

--- quickstats_api.py
+++ quickstats_api.py
@@ -8,12 +8,14 @@ BASE_URL = "https://quickstats.nass.usda.gov/api/api_GET/"
  #%%Loading State abbreviations
 states = pd.read_csv("states.csv")
 state_abbrevs = states[['Abbreviation']]
-# %% Creates dfs for each state
+# %%
 for state in state_abbrevs['Abbreviation']:
     params = {
         "key": API_KEY,
-        "state_alpha": state,
-        "statisticcat_desc": "YIELD",
+        "sector_desc": "CROPS",
+        "commodity_desc" : "CROP TOTALS",
+
+        "state_alpha" : state,
         "format": "JSON"
     }
     
@@ -28,20 +30,13 @@ for state in state_abbrevs['Abbreviation']:
             print(f"No data returned for {state}")
     else:
         print(f"Error fetching data for {state}: {response.status_code}")
+# %%
+state_abbrevs = state_abbrevs.drop(state_abbrevs[state_abbrevs['Abbreviation'] == 'DC'].index)
 
-# %%
+
+state_dfs = {}
 for state in state_abbrevs['Abbreviation']:
-    params = {
-    "key": API_KEY,
-    "state_alpha": state,
-    "statisticcat_desc": "YIELD",
-    "format": "JSON",
-    }
-    response = requests.get(BASE_URL, params=params)
-    if response.status_code == 200:
-        data = response.json()
-        df = pd.DataFrame(data['data'])
-        df.to_csv(f"nass_state_yield_{state}.csv", index=False)
-    else:
-        print(f"Error: {response.status_code}")
+    df = pd.read_csv(f"nass_yield_{state}.csv")
+    state_dfs[state] = df[['load_time', 'county_ansi', 'Value', 'county_name', 'county_code']]
 # %%
+state_dfs['CA'].head()
